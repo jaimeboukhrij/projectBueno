81efb3392eddb7ba0435a6644eceda4f4853a08f
jaimeboukhrij <jaimeboukhrijperez@gmail.com>
2023-05-17 11:02:25 +0200

    primero

--- public/js/script.js
+++ public/js/script.js
@@ -1,33 +1,14 @@
-// https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("Project2 JS imported successfully!");
-});
+let arrow = document.querySelectorAll(".arrow");
+for (var i = 0; i < arrow.length; i++) {
+  arrow[i].addEventListener("click", (e) => {
+    let arrowParent = e.target.parentElement.parentElement;
+    arrowParent.classList.toggle("showMenu");
+  });
+}
 
-
-
-const body = document.querySelector('body'),
-  sidebar = body.querySelector('nav'),
-  toggle = body.querySelector(".toggle"),
-  searchBtn = body.querySelector(".search-box"),
-  modeSwitch = body.querySelector(".toggle-switch"),
-  modeText = body.querySelector(".mode-text");
-
-
-toggle.addEventListener("click", () => {
+let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".bx-menu");
+console.log(sidebarBtn);
+sidebarBtn.addEventListener("click", () => {
   sidebar.classList.toggle("close");
-})
-
-searchBtn.addEventListener("click", () => {
-  sidebar.classList.remove("close");
-})
-
-modeSwitch.addEventListener("click", () => {
-  body.classList.toggle("dark");
-
-  if (body.classList.contains("dark")) {
-    modeText.innerText = "Light mode";
-  } else {
-    modeText.innerText = "Dark mode";
-
-  }
 });
--- routes/trip.routes.js
+++ routes/trip.routes.js
@@ -27,6 +27,43 @@ router.post("/createTrip", (req, res, next) => {
 })
 
 
+router.get("/seachTrip/:date/:origin/:destination/:idOrigin/:idDestination", (req, res, next) => {
+  const { idDestination, idOrigin, date } = req.params
+  Trip
+    .find({ $and: [{ "origin.id": { $eq: idOrigin } }, { "destination.id": { $eq: idDestination } }, { "departureDay": { $eq: date } }] })
+    .populate({
+      path: "passengers",
+      select: 'name'
+    })
+    .populate({
+      path: "owner",
+      select: 'name'
+    })
+    .then(trip => {
+      if (trip.length > 0) { res.render("trip/list-trip", { trip }) }
+      else { (res.send("no hay viaje")) }
+    })
+    .catch(err => next(err))
+})
+
+router.post("/seachTrip/:date/:origin/:destination/:idOrigin/:idDestination", (req, res, next) => {
+  const { idTrip } = req.body
+  const { _id: newPass } = req.session.currentUser
+  Trip
+    .findById(idTrip)
+    .then(trip => {
+      const newPassengers = [...trip.passengers, newPass]
+      Trip.findByIdAndUpdate(idTrip, { passengers: newPassengers })
+        .then(() => res.redirect("/"))
+      //trip.passengers.push(passengers)
+    })
+
+    .catch(err => next(err))
+})
+
+
+
+
 
 
 module.exports = router
